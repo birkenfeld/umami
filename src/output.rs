8fe804a6ff456edcb81a05bacaa440269b447344
Georg Brandl <g.brandl@fz-juelich.de>
2026-03-25 13:41:51 +0100

    add file writer using rkyv

--- src/output.rs
+++ src/output.rs
@@ -1,9 +1,11 @@
 // Part of the Unified Mechanism for Acquisition of Measured Intensity
 // (UMAMI), see README and LICENSE files for more info.
 
+use std::{fs::File, io::BufWriter, path::PathBuf};
 use anyhow::{anyhow, Context};
 use hdf5;
 use itertools::Itertools;
+use rkyv::{api::high::to_bytes_in, ser::writer::IoWriter};
 use crate::lprintln;
 use crate::channel::{Receiver, Sender};
 use crate::config::OutputConfig;
@@ -279,7 +281,53 @@ impl Output for HDF5EventsOutput {
 // Broadcast over udp/uds
 // pub struct UDPOutput;
 // pub struct UDSOutput;
-// pub struct FileOutput;
+
+pub struct FileOutput {
+    // Configuration
+    dir: PathBuf,
+    filename: Option<String>,
+    // Runtime
+    writer: Option<IoWriter<BufWriter<File>>>,
+}
+
+impl Output for FileOutput {
+    fn from_config(config: toml::Table) -> UResult<Self> where Self: Sized {
+        let dir = config.get("dir")
+            .ok_or_else(|| anyhow!("Missing 'dir' in file output config"))?
+            .as_str()
+            .ok_or_else(|| anyhow!("'dir' in file output config must be a string"))?;
+        Ok(FileOutput { writer: None, filename: None, dir: PathBuf::from(dir) })
+    }
+
+    // TODO: config api to set the filename
+
+    fn handle_start_of_run(&mut self, _run: &str) -> UResult<()> {
+        if let Some(filename) = &self.filename {
+            let path = self.dir.join(filename);
+            let file = File::create(&path)
+                .with_context(|| format!("Creating output file {}", path.display()))?;
+            self.writer = Some(IoWriter::new(BufWriter::with_capacity(1 << 20, file)));
+        } else {
+            self.writer = None;
+        }
+        Ok(())
+    }
+
+    fn handle_end_of_run(&mut self) -> UResult<()> {
+        self.writer = None;
+        Ok(())
+    }
+
+    fn handle_events(&mut self, events: &[Event]) -> UResult<()> {
+        if let Some(mut writer) = self.writer.as_mut() {
+            for event in events {
+                to_bytes_in::<_, rkyv::rancor::Failure>(event, &mut writer)
+                    .context("Serializing event for file output")?;
+            }
+        }
+        Ok(())
+    }
+}
 
 
 pub fn start(config: OutputConfig, common: OutputCommon) -> UResult<()> {
@@ -287,6 +335,7 @@ pub fn start(config: OutputConfig, common: OutputCommon) -> UResult<()> {
         "none" => Ok(NullOutput::from_config(config.config)?.start(common)?),
         "diag" => Ok(DiagOutput::from_config(config.config)?.start(common)?),
         "hdf5" => Ok(HDF5EventsOutput::from_config(config.config)?.start(common)?),
+        "file" => Ok(FileOutput::from_config(config.config)?.start(common)?),
         _ => Err(anyhow!("Unknown output type: {}", config.r#type).into()),
     }
 }
