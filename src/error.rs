83d78995b7796c9ad07da9c50e8911f8c91daf02
Georg Brandl <g.brandl@fz-juelich.de>
2026-08-09 10:23:51 +0200

    fix doubl error formatting

--- src/error.rs
+++ src/error.rs
@@ -7,8 +7,32 @@ pub type UResult<T> = Result<T, UError>;
 
 #[derive(Debug, Error)]
 pub enum UError {
-    #[error("{0:#}")]
+    #[error(transparent)]
     Other(#[from] anyhow::Error),
     #[error("Data from file exhausted")]
     NoMoreData,
+}
+
+#[cfg(test)]
+mod tests {
+    use anyhow::Context;
+
+    #[test]
+    fn chain_is_not_duplicated_across_a_context_rewrap() {
+        fn inner() -> super::UResult<()> {
+            Err(std::io::Error::from_raw_os_error(2))
+                .with_context(|| "Opening source file \"x\"".to_string())?
+        }
+        fn outer() -> super::UResult<()> {
+            inner().context("Error setting parameter replay_file")?;
+            Ok(())
+        }
+
+        let message = format!("{:#}", outer().unwrap_err());
+        assert_eq!(
+            message,
+            "Error setting parameter replay_file: Opening source file \"x\": \
+             No such file or directory (os error 2)"
+        );
+    }
 }
